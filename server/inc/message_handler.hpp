40d308a8929655a60912bc70960a6e77f19bdd20
Adesh Narke <adeshnarke20@gmail.com>
2026-04-15 19:15:05 +0530

    added build/run scripts, testing setup, utils module, and code formatting support

--- server/inc/message_handler.hpp
+++ server/inc/message_handler.hpp
@@ -1,9 +1,10 @@
 #ifndef MESSAGE_HANDLER_H
 #define MESSAGE_HANDLER_H
 
-#include <string>
 #include "client_manager.hpp"
+#include <string>
 
-void process_message(const std::string& message, int sender_socket, ClientManager& manager);
+void process_message(const std::string &message, int sender_socket,
+                     ClientManager &manager);
 
 #endif
--- server/src/client_manager.cpp
+++ server/src/client_manager.cpp
@@ -1,55 +1,56 @@
 #include "client_manager.hpp"
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <iostream>
-#include<arpa/inet.h>
+#include <unistd.h>
 
-void ClientManager::add_client(int socket, const std::string& username) {
-    std::lock_guard<std::mutex> lock(mtx);
-    clients[socket] = {socket, username};
+void ClientManager::add_client(int socket, const std::string &username) {
+  std::lock_guard<std::mutex> lock(mtx);
+  clients[socket] = {socket, username};
 }
 
 void ClientManager::remove_client(int socket) {
-    std::lock_guard<std::mutex> lock(mtx);
-    clients.erase(socket);
+  std::lock_guard<std::mutex> lock(mtx);
+  clients.erase(socket);
 }
 
-void ClientManager::broadcast(const std::string& message, int sender_socket) {
-    std::lock_guard<std::mutex> lock(mtx);
+void ClientManager::broadcast(const std::string &message, int sender_socket) {
+  std::lock_guard<std::mutex> lock(mtx);
 
-    for (auto& [sock, client] : clients) {
-        if (sock != sender_socket) {
-            send(sock, message.c_str(), message.size(), 0);
-        }
+  for (auto &[sock, client] : clients) {
+    if (sock != sender_socket) {
+      send(sock, message.c_str(), message.size(), 0);
     }
+  }
 }
 
-int ClientManager::get_socket_by_username(const std::string& username) {
-    std::lock_guard<std::mutex> lock(mtx);
+int ClientManager::get_socket_by_username(const std::string &username) {
+  std::lock_guard<std::mutex> lock(mtx);
 
-    for (auto& [sock, client] : clients) {
-        if (client.username == username) {
-            return sock;
-        }
+  for (auto &[sock, client] : clients) {
+    if (client.username == username) {
+      return sock;
     }
-    return -1;
+  }
+  return -1;
 }
 
 std::string ClientManager::get_username(int socket) {
-    std::lock_guard<std::mutex> lock(mtx);
-    return clients[socket].username;
+  std::lock_guard<std::mutex> lock(mtx);
+  return clients[socket].username;
 }
 
-bool ClientManager::username_exists(const std::string& username) {
-    for (auto& [sock, user] : clients) {
-        if (user.username == username) return true;
-    }
-    return false;
+bool ClientManager::username_exists(const std::string &username) {
+  for (auto &[sock, user] : clients) {
+    if (user.username == username)
+      return true;
+  }
+  return false;
 }
 
 std::string ClientManager::get_all_users() {
-    std::string list = "Online Users:\n";
-    for (auto& [sock, user] : clients) {
-        list += "- " + user.username + "\n";
-    }
-    return list;
+  std::string list = "Online Users:\n";
+  for (auto &[sock, user] : clients) {
+    list += "- " + user.username + "\n";
+  }
+  return list;
 }
